acc9f3149a08d1d8036ce3df2c5888a51fbd6d27
ykk <ykk@qq.com>
2022-11-04 10:11:27 +0800

    commit

--- redis/bloom/data/bloom.go
+++ redis/bloom/data/bloom.go
@@ -1,11 +1,3 @@
-/*
- * @Author: ykk ykk@qq.com
- * @Date: 2022-05-28 14:34:11
- * @LastEditors: ykk ykk@qq.com
- * @LastEditTime: 2022-11-03 17:19:47
- * @FilePath: /allfunc/redis/bloom/data/bloom.go
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 package data
 
 import (
@@ -29,7 +21,7 @@ func (b *Bloom) Add(str string) error {
 	var err error
 	for _, f := range b.HashFuncs {
 		offset := f(str)
-		fmt.Println(offset)
+		fmt.Println("位置：", offset)
 		err := b.Conn.SetBit(context.Background(), b.Key, int64(offset), 1).Err()
 		if err != nil {
 			return err
